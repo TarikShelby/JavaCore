ad5c80b67df936771a9fba3813a954ced4139b79
Shelby <>
2021-08-28 14:41:02 +0300

    IF  = Complete

---
+++ src/main/java/scan/NewScan.java
@@ -0,0 +1,22 @@
+package scan;
+
+import java.util.Scanner;
+
+public class NewScan {
+    public static void main(String[] args) {
+
+
+        Scanner name = new Scanner(System.in);// обязательно проверьте что бы подключился java util
+        System.out.println(" Будете что-то писать : ");
+        String id = name.nextLine();
+
+        System.out.println("Вы написали : " + id); // сделали эхо сканера
+        System.out.println("Что-то с название компании : ");
+        String username = name.nextLine();
+        System.out.println(" тут можно только цифры : ");
+        int password = name.nextInt();
+        System.out.println(username + " " + password);
+    }
+
+
+}
