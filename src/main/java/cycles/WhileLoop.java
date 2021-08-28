cb04ff480b8bfd777ced61eb805439a52b9312f1
Shelby <>
2021-08-28 13:50:53 +0300

    While Commit

---
+++ src/main/java/cycles/WhileLoop.java
@@ -0,0 +1,28 @@
+package cycles;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value <= 1) {
+            System.out.println(" Beggin ");
+            value++;
+        }
+        int champ = 0;
+        while (champ < 2) {
+            System.out.println("И : " + champ);
+            champ++;
+        }
+
+
+        System.out.println("YOOOOOOOOOOOOOOOOOOOOOUUUUUUUUUUUUUUU");
+        int moor = -3;
+        while (moor < 0) {
+            System.out.println(moor);
+            moor++;
+
+        }
+        System.out.println("youuu poter9li begi ");
+    }
+
+
+}
