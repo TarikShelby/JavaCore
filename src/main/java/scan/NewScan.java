ecbf3a7287f483416078d1773f81109f3a772846
Shelby <>
2021-08-28 14:41:36 +0300

    SCANNER

--- src/main/java/scan/NewScan.java
+++ src/main/java/scan/NewScan.java
@@ -4,12 +4,9 @@ import java.util.Scanner;
 
 public class NewScan {
     public static void main(String[] args) {
-
-
         Scanner name = new Scanner(System.in);// обязательно проверьте что бы подключился java util
         System.out.println(" Будете что-то писать : ");
         String id = name.nextLine();
-
         System.out.println("Вы написали : " + id); // сделали эхо сканера
         System.out.println("Что-то с название компании : ");
         String username = name.nextLine();
