35d9393f18cf79af985939789b660dfb3983d3fc
Shelby <>
2021-08-28 13:13:28 +0300

    Strings ez level

---
+++ src/main/java/strings/ReferenceString.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class ReferenceString {
+    public static void main(String[] args) {
+        int x = 30;
+        String name = "Tayson";
+        String space = " ";
+        String win = "rounds K.O";
+        String ez = "Выграл ";
+        System.out.println(name + space + ez + x + space + win);
+
+        String l1 = "helloo33333333333333333333333";
+        System.out.println(l1.length());
+    }
+}
