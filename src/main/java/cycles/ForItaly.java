9487bbd39b424c3a42b612263aa1f6260f4e7eef
Shelby <>
2021-08-28 14:01:11 +0300

    "For" it`s ez, no problem everybody.

---
+++ src/main/java/cycles/ForItaly.java
@@ -0,0 +1,13 @@
+package cycles;
+
+public class ForItaly {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("Наш Проект 33" + x);
+        }
+        for (int y = 0; y <= 10; y++) {
+            System.out.println("Наш проект на другой платформе под номером № : " + y);
+
+        }
+    }
+}
