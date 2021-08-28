d7c28f81987c0ae779737a8f482c209060655d8e
Shelby <>
2021-08-28 12:30:49 +0300

    Variables

--- src/main/java/first/code/helloworld/Hell.java
+++ src/main/java/first/code/helloworld/Hell.java
@@ -1,8 +1,8 @@
-package first.code.helloworld;
-
-public class Hell {
-    public static void main(String[] args) {
-        System.out.println(" Hello World ");//this is the first program of all programmers
-
-    }
-}
+//package first.code.helloworld;
+//
+//public class Hell {
+//    public static void main(String[] args) {
+//        System.out.println(" Hello World ");//this is the first program of all programmers
+//
+//    }
+//}
---
+++ src/main/java/variables/Variables.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myInt; // -2147483648 до 2147483647 , 4 байта
+        byte myByte = 127; //от 127  -128 ,1 байт
+        short myShort = 1998; //от -32768 до 32767 , 2 байта
+        long myLong = 123456789; // от -9223372036854775808 до 9223372036854775807 ,8 байтов памяти
+        double myDouble = 321.213;
+        float myFloat = 321.241f;
+        char A = 'a';
+        boolean True = true;
+        var myVar = 1243412433;// new variables
+        //these are all variables
+        myInt = 31;
+        {
+            System.out.println(myInt);
+        }
+        System.out.println(myByte);
+        System.out.println(myDouble);
+        System.out.println(A);
+        System.out.println(True);
+    }
+
+}
