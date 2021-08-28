9a1dc3f1665c3e2cc32e154ffd20460aec72af68
Shelby <>
2021-08-28 15:11:51 +0300

    DO WHILE it`s commit for u

---
+++ src/main/java/cycles/DoWhile.java
@@ -0,0 +1,23 @@
+package cycles;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lemonade;
+        do {
+            System.out.println("Угадай число до 10 : ");
+            lemonade = scan.nextInt();
+        } while (lemonade != 8);
+        System.out.println("Угадал 8");
+        int x = 8;
+        do {
+            System.out.println(" " + x);
+            x++;
+        } while (x < 10);
+    }
+
+
+}
+
