c8b9d364fc765066f157c08978679c3120855e4a
Shelby <>
2021-08-29 15:44:22 +0300

    OBJECT

--- src/main/java/ClassAndObject/Klass.java
+++ src/main/java/ClassAndObject/Klass.java
@@ -8,6 +8,7 @@ public class Klass {
         dog.voice = "Gav";
         dog.color = "white";
         System.out.println(" Меня зовут " + dog.name + " мне " + dog.age + " я могу " + dog.voice + " Цвет настроение " + dog.color);
+        dog.speak();
         Animal cat = new Animal();
         cat.name = "markis";
         cat.age = 3;
@@ -15,6 +16,7 @@ public class Klass {
         cat.color = "Black";
         System.out.println(cat.voice);
         cat.speak(); // мы создали в обычном классе метод спик
+        cat.br();
     }
 
     static class Animal {
@@ -22,9 +24,17 @@ public class Klass {
         int age;
         String voice;
         String color;
-        void speak(){
-            System.out.println( " " + name );
+
+        void speak() {
+            System.out.println(" " + name);
         }
+
+        void br() {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(name);
+            }
         }
-    }//Классов может быть много ,точка входа одна
+    }
+}
+//Классов может быть много ,точка входа одна
 
