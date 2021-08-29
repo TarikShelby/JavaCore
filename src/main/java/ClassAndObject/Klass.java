8aa62f22979575757989a1266b35fceab6e1ba9d
Shelby <>
2021-08-29 14:53:27 +0300

    Class with animals

---
+++ src/main/java/ClassAndObject/Klass.java
@@ -0,0 +1,26 @@
+package ClassAndObject;
+
+public class Klass {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.name = "Peslik";
+        dog.age = 5;
+        dog.voice = "Gav";
+        dog.color = "white";
+        System.out.println(" Меня зовут " + dog.name + " мне " + dog.age + " я могу " + dog.voice + " Цвет настроение " + dog.color );
+        Animal cat = new Animal();
+        cat.name = "markis";
+        cat.age = 3;
+        cat.voice = "M9y ";
+        cat.color = "Black";
+        System.out.println(cat.voice);
+    }
+
+    static class Animal {
+        String name;
+        int age;
+        String voice;
+        String color;
+
+    }//Классов может быть много ,точка входа одна
+}
