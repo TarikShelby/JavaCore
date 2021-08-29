2085e398a03a2bb7786b4f113af65a50a3dde660
Shelby <>
2021-08-29 13:08:26 +0300

    Multi Array

---
+++ src/main/java/Arrays/Multi_Arrays.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Multi_Arrays {
+    public static void main(String[] args) {
+        int[][] ym = {{1, 2, 3, 4}, {5, 6, 7, 8, 9}, {1}, {5, 3, 1}};
+        for (int i = 0; i < ym.length; i++) {
+            for (int j = 0; j < ym[i].length; j++) {
+                System.out.println(ym[i][j]);
+            }
+        }
+    }
+}
