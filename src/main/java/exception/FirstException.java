3cbd07500f6ec7992f94ed63b87ed394d42d0b71
Shelby <>
2021-09-02 00:15:43 +0300

    Exception fileNotFound

---
+++ src/main/java/exception/FirstException.java
@@ -0,0 +1,22 @@
+package exception;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FirstException {
+    public static void main(String[] args) {/*throws FileNotFoundException , даем понять java что может быть такая ошибка
+                                                                            мы создаем файл которого может не существовать  */
+        File file = new File("aabb");
+
+        {
+            try {
+                Scanner scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File не найден");
+            }
+        }
+    }
+
+}
